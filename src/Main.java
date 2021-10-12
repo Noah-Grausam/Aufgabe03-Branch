1c847cbf41547de9673ecf39afde7dbb5699c58a
Noah-Noah <crafterniek@gmail.com>
2021-10-12 14:59:40 +0200

    Added basic structure

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ GitHub: https://github.com/Noah-Grausam/Aufgabe03-Branch
 */
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,9 +22,38 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root, 600, 600);
+        Scene scene = new Scene(root, 200, 200);
         stage.setTitle("Temp. Converter");
 
+        VBox vBox = new VBox();
+
+        Label l1 = new Label("Grad Celsius");
+        TextField tf1 = new TextField();
+
+        Label l2 = new Label("Grad Fahrenheit");
+        TextField tf2 = new TextField();
+
+        EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        };
+        EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        };
+
+        Button b1 = new Button("Convert C -> F");
+        b1.setOnAction(event1);
+        Button b2 = new Button("Convert F -> C");
+        b2.setOnAction(event2);
+
+        vBox.getChildren().addAll(l1, tf1, l2, tf2, b1, b2);
+
+        root.getChildren().add(vBox);
         stage.setScene(scene);
         stage.show();
     }
